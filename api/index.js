73871732404afc9f6b35602c46d3ef13d925cdd4
Nguyễn Minh Thông <145902006+minhthong2k3@users.noreply.github.com>
2025-10-23 10:26:26 +0700

    create user api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
@@ -12,3 +13,5 @@ const app = express();
 app.listen(3000, () => {
     console.log('API server running on http://localhost:3000');
 });
+app.use('/api/user', userRoutes);
+
