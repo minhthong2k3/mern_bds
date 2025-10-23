5aaee9b143dfde1f7c698b9714d30f49dd6ed727
Nguyễn Minh Thông <145902006+minhthong2k3@users.noreply.github.com>
2025-10-23 12:45:33 +0700

    finish sign up ui page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div>
+      <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+      <form action="">
+        <input type="text" placeholder='Username' className='border border-gray-400 rounded-md px-3 py-2 w-full max-w-md block mx-auto my-3'
+        id = 'username'/>
+        <input type="email" placeholder='Email' className='border border-gray-400 rounded-md px-3 py-2 w-full max-w-md block mx-auto my-3'
+        id = 'email'/>
+        <input type="password" placeholder='Password' className='border border-gray-400 rounded-md px-3 py-2 w-full max-w-md block mx-auto my-3'
+        id = 'password'/>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded-md w-full max-w-md block mx-auto my-3 uppercase hover:bg-blue-600 disabled:bg-blue-300'>Sign Up</button> 
+      </form>
+      <div className='text-center mt-4'>
+        <span>Already have an account? </span>
+        <Link to ={"/sign-in"}>
+        <span className='text-blue-500 hover:underline'>Sign In</span>
+        </Link>
+      </div>
+    </div>
   )
 }
