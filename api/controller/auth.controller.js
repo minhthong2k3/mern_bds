894f901c344764e748f39e5024668c421dc7c2b7
Nguyễn Minh Thông <145902006+minhthong2k3@users.noreply.github.com>
2025-10-28 12:32:12 +0700

    sign-in route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { handleError } from '../ultis/error.js';
+import jwt from 'jsonwebtoken';
+import e from 'express';
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   // Here you would typically add logic to save the user to the database
@@ -17,3 +19,23 @@ export const signup = async (req, res, next) => {
 
   
 };
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(handleError(401, 'Invalid password'));
+    } 
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie("access_token", token, { httpOnly: true}).status(200).json(rest);
+    
+  } catch (error) {
+    next(error);
+  }
+};
+
