a1c27c5554b61fd558390bb7093801776cb03d32
Nguyễn Minh Thông <145902006+minhthong2k3@users.noreply.github.com>
2025-11-05 16:01:20 +0700

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+// src/redux/store.js
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { user:userReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
-  }),
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      // tránh cảnh báo serializable khi dùng redux-persist
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
